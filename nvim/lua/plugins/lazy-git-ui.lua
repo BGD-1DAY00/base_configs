b14c79e1bd5f7765bcc3779b1b7a62a06f7d8232
BGD-1DAY00 <saadosman450@gmail.com>
2025-06-24 00:55:26 -0500

    updates

--- nvim/lua/plugins/lazy-git-ui.lua
+++ nvim/lua/plugins/lazy-git-ui.lua
@@ -1,26 +1,26 @@
 return {
-  "kdheepak/lazygit.nvim",
-  cmd = {
-    "LazyGit",
-    "LazyGitConfig",
-    "LazyGitCurrentFile",
-    "LazyGitFilter",
-    "LazyGitFilterCurrentFile",
-  },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  keys = {
-    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-    { "<leader>lG", "<cmd>LazyGitConfig<cr>", desc = "LazyGit Config" },
-  },
-  config = function()
-    require("lazygit").setup({
-      floating_window_winblend = 0,
-      floating_window_scaling_factor = 0.9,
-      floating_window_border_chars = {'╭','─', '╮', '│', '╯','─', '╰', '│'},
-      floating_window_use_plenary = 0,
-      use_neovim_remote = 1,
-    })
-  end,
+	"kdheepak/lazygit.nvim",
+	cmd = {
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFilterCurrentFile",
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	keys = {
+		{ "<leader>lg", "<cmd>LazyGit<cr>",       desc = "LazyGit" },
+		{ "<leader>lG", "<cmd>LazyGitConfig<cr>", desc = "LazyGit Config" },
+	},
+	config = function()
+		require("lazygit").setup({
+			floating_window_winblend = 0,
+			floating_window_scaling_factor = 0.9,
+			floating_window_border_chars = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+			floating_window_use_plenary = 0,
+			use_neovim_remote = 1,
+		})
+	end,
 }
--- nvim/lua/plugins/toggle-term.lua
+++ nvim/lua/plugins/toggle-term.lua
@@ -11,5 +11,11 @@ return {
       start_in_insert = true,
       direction = 'float',
     })
+    
+    -- Key mappings for multiple floating terminals
+    vim.keymap.set('n', '<leader>t1', '<cmd>1ToggleTerm direction=float<cr>', { desc = 'Terminal 1' })
+    vim.keymap.set('n', '<leader>t2', '<cmd>2ToggleTerm direction=float<cr>', { desc = 'Terminal 2' })
+    vim.keymap.set('n', '<leader>t3', '<cmd>3ToggleTerm direction=float<cr>', { desc = 'Terminal 3' })
+    vim.keymap.set('n', '<leader>t4', '<cmd>4ToggleTerm direction=float<cr>', { desc = 'Terminal 4' })
   end,
 }
